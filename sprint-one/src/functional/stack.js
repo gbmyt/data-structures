119ff26c95e5cd006fbc4f000a947d9b7417a07d
gbmyt <gbmytabc1@gmail.com>
2022-09-12 23:03:06 -0400

    Complete functional stack and queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0;
 
-  // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.pop = function() {
+    var newest = Math.max(...Object.keys(storage));
+    var popped = storage[newest];
+    delete storage[newest];
+
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
