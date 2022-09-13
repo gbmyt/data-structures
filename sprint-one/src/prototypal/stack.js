9595b56578954c5f6b5054f5a8c8ee047d6e9182
gbmyt <gbmytabc1@gmail.com>
2022-09-12 23:29:50 -0400

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.index = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+stackMethods.pop = function() {
+  var newest = Math.max(...Object.keys(this.storage));
+  var popped = this.storage[newest];
+  delete this.storage[newest];
+  return popped;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
